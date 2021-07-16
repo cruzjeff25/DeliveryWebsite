dbf1d2425b28286bd35d365688b5e042dc41b4bc
cruzjeff25 <cruzjeff254@gmail.com>
2021-07-16 19:51:15 +0800

    updated

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
 function toggleMenu(){
-    const menu = document.querySelector('.nav_bar');
-    menu.classList.toggle('active');
+    const menu = document.querySelector('.nav_bar')
+    menu.classList.toggle('active')
 }
 
 function nightMode(){
-    const mode = document.querySelector('.container');
-    mode.classList.toggle('nightmode');
+    const mode = document.querySelector('.container')
+    mode.classList.toggle('nightmode')
 }
 
 function signUp(){
-    const sign = document.querySelector('.modal_container');
+    const sign = document.querySelector('.modal_container')
     sign.classList.toggle('show')
+}
+
+function standard(){
+    let book = document.querySelector('.standard_container')
+    book.classList.toggle('show')
+}
+
+function premium(){
+    const book = document.querySelector('.premium_container')
+    book.classList.toggle('show')
 }
